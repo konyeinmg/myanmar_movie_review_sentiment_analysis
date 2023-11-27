160fc757ca4c22a5bd406a8696f52927108a5ce4
konyeinmg <nyeinchanaung.myeik@gmail.com>
2023-11-27 08:53:16 +0630

    split train test method

--- services.py
+++ services.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+
 
 def read_data(url):
     f = open(url)
@@ -8,6 +10,23 @@ def read_data(url):
     neg_reviews = data['neg']
 
     return pos_reviews, neg_reviews
+
+def train_test_split(train_percent, pos, neg):
+    train_pos_len = int(len(pos) * train_percent)
+    train_neg_len = int(len(neg) * train_percent)
+
+    train_pos = pos[:train_pos_len]
+    train_neg = neg[:train_neg_len]
+    test_pos = pos[train_pos_len:]
+    test_neg = neg[train_neg_len:]
+
+    train_x = train_pos + train_neg
+    test_x = test_pos + test_neg
+
+    train_y = np.append(np.ones(len(train_pos)), np.zeros(len(train_neg)))
+    test_y = np.append(np.ones(len(test_pos)), np.zeros(len(test_neg)))
+
+    return train_x, test_x, train_y, test_y
 
 '''
 #main method is just to verify the upper methods
--- training.py
+++ training.py
@@ -1,26 +1,11 @@
-import numpy as np
-
 import services
 import preprocess
 import naivebayes
 
 pos,neg = services.read_data('data.json')
 
-train_percent = 0.75
+train_x, test_x, train_y, test_y = services.train_test_split(0.75, pos, neg)
 
-train_pos_len = int(len(pos) * train_percent)
-train_neg_len = int(len(neg) * train_percent)
-
-train_pos = pos[:train_pos_len]
-train_neg = neg[:train_neg_len]
-test_pos = pos[train_pos_len:]
-test_neg = neg[train_neg_len:]
-
-train_x = train_pos + train_neg
-test_x = test_pos + test_neg
-
-train_y = np.append(np.ones(len(train_pos)), np.zeros(len(train_neg)))
-test_y = np.append(np.ones(len(test_pos)), np.zeros(len(test_neg)))
 
 #print(len(train_x))
 #print(len(test_x))
